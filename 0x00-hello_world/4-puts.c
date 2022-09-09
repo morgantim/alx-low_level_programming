0c83c29e1a1e773b9f645827c714d855bebec0d2
morgantim <109325718+morgantim@users.noreply.github.com>
2022-09-09 05:38:09 +0300

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
+
 /**
-*main - Entry point
-*
-*Return: always 0
-*/
+ *
+ *  *  main -Entry point
+ *
+ *   *
+ *
+ *    *  Return: Always 0 (Success)
+ *
+ *     */
+
 int main(void)
+
 {
 
-       puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
 }
