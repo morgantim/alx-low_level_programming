c9eaa560683b16c80b1d0df133c3a2ba5328e58a
morgantim <109325718+morgantim@users.noreply.github.com>
2022-09-09 05:27:05 +0300

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,5 @@
-#include <stdio.h>
-/**
- * main - Entry point
- *
- * Return: always 0
- */
-int main(void)                                    {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-        return (0);
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
