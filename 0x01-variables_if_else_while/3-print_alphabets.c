873264339e8360b384f916e9e5fe348a6a2598ed
morgantim <109325718+morgantim@users.noreply.github.com>
2022-09-12 22:05:26 +0300

    3-print

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+include <stdio.h>
+
+/**
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	for (letter = 'A'; letter <= Z; letter++)
+		putchar(letter);
+	putchar('\n');
+	return (0);
+}
