24b305e1887c655086d95a39ab8b1413940e33a9
morgantim <109325718+morgantim@users.noreply.github.com>
2022-09-13 05:24:27 +0300

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Print the lowercase aphabet in reverse.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+	putchar('\n');
+	return (0);
+}
