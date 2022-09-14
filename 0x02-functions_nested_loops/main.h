d86f787e16eaaa09a7c27f70ec9f6578a8b8106c
morgantim <109325718+morgantim@users.noreply.github.com>
2022-09-14 12:16:26 +0300

    Main

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
+
+int _putchar(char);
+void print_alphabet(void);
+void print_alpabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void time_table(void);
+int add(int a, int b);
+void print_to_98(int n);
+void num(int r, int n);
+
+#endif
