1c6890dd1362f11e2e3a073c2bad7ffab62ad7e6
morgantim <109325718+morgantim@users.noreply.github.com>
2022-09-16 06:18:20 +0300

    Mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * mul - this function multiplies two values
+ *
+ * @a: character value
+ * @b: character value
+ *
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+
+}
