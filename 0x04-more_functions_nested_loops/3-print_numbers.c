69be212b4a7c01d42589d6015bd4b9ec79c7861b
morgantim <109325718+morgantim@users.noreply.github.com>
2022-09-16 06:19:13 +0300

    Print

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - this function multiplies two values
+ *
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		_putchar('0' + n);
+	}
+		_putchar('\n');
+}
