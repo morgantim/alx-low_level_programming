44e7119f064396b51a607ec90fc08ebf1c9a1b84
morgantim <109325718+morgantim@users.noreply.github.com>
2022-10-01 03:24:52 +0300

    1args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - this program prints its name
+ *
+ * @argc: number of arguments
+ * @argv: string of arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
