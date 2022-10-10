825c9a42ab1ad04e5e7645124c9e7c20c6def3e1
morgantim <109325718+morgantim@users.noreply.github.com>
2022-10-10 14:28:39 +0300

    New

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,25 +1,83 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
+
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+dog_t *new_dog(char *name, float age, char *owner);
 /**
- * print_dog - this funcion prints dog nfo
- * @d: type struct
- * dog - struct name
+ * *_strcpy - this function return the legth of a string
+ *
+ * @dest: where the source need to be copied
+ * @src: source of the string
+ *
+ * Return: Dest ponter
  */
-void print_dog(struct dog *d)
+char *_strcpy(char *dest, char *src)
 {
-	if (d == NULL)
-		return;
-	if (!d->name)
-		printf("Name: (nil)\n");
-	else
-		printf("Name: %s\n", d->name);
-	if (d->age < 0)
-		printf("Age: (nil)\n");
-	else
-		printf("Age: %.6f\n", d->age);
-	if (!d->owner)
-		printf("Owner: (nil)\n");
-	else
-		printf("Owner: %s\n", d->owner);
+	int length;
+
+	for (length = 0; src[length] != '\0'; length++)
+	{
+		dest[length] = src[length];
+	}
+	dest[length] = src[length];
+	return (dest);
+}
+/**
+ * _strlen - this function return the legth of a string
+ *
+ * @s: value
+ *
+ * Return: value
+ */
+int _strlen(char *s)
+{
+	char n = 'a';
+	int i;
+
+	for (i = 0; (n != '\0'); i++)
+	{
+		n = s[i];
+	}
+	return (i - 1);
+}
+/**
+ * new_dog - this funcion prints dog nfo
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Return: struct dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *newerDog;
+
+	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
+
+	newerDog = malloc(sizeof(dog_t));
+	if (newerDog == NULL)
+		return (NULL);
+
+	newerDog->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if (newerDog->name == NULL)
+	{
+		free(newerDog);
+		return (NULL);
+	}
+
+	newerDog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (newerDog->owner == NULL)
+	{
+		free(newerDog->name);
+		free(newerDog);
+		return (NULL);
+	}
+
+	newerDog->name = _strcpy(newerDog->name, name);
+	newerDog->age = age;
+	newerDog->owner = _strcpy(newerDog->owner, owner);
+
+	return (newerDog);
 }
