8059a62591bdb06289afeeaa37f2b3c06d8d30a8
morgantim <109325718+morgantim@users.noreply.github.com>
2022-10-10 14:20:20 +0300

    Dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H
+#define _DOG_H
+/**
+ * struct dog - struct of dog data
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+void free_dog(dog_t *d);
+#endif
